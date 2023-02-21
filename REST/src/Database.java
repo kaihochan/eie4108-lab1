d16fee9156942624dd52ed861bd66d3606bbb70d
IATNEHC <98003863+IATNEHC@users.noreply.github.com>
2023-02-21 16:47:49 +0800

    Updated Database.java

--- REST/src/Database.java
+++ REST/src/Database.java
@@ -1,18 +1,40 @@
-import javax.ws.rs.Path;
-import com.sun.jersey.spi.resource.Singleton;
 import java.util.Hashtable;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.Gson;
+import com.sun.jersey.spi.resource.Singleton;
 
 @Singleton
 @Path("/mark")
 public class Database {
 	private Hashtable<String,Integer> table = new Hashtable<String,Integer>();
-
-	public String addRecord(. . .) {
+	
+	@POST
+	@Path("{student}+{mark}")
+	public String addRecord(@PathParam("student") String student, @PathParam("mark") String mark) {
 	// Add a record using id as key and mark as value
+		table.put(student, Integer.valueOf(mark));
+		System.out.println("Student: " + student);
+		System.out.println("Mark: " + mark);
+		return "";
 	}
 	
-	public String getMark(. . .) {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMark(@QueryParam("student") String student) {
 	// Retrieve a record using id as key
+		if(table.contains(student)) {
+			System.out.println(student + " " + table.get(student));
+			return Integer.toString(table.get(student));
+		}
+		System.out.println("Student not exists");
+		return "";
 	}
 	
 	public String updateRecord(. . .) {
