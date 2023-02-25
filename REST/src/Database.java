a82af6c9aa47f15725e916c089871c0f42bb1f21
IATNEHC <98003863+IATNEHC@users.noreply.github.com>
2023-02-26 00:33:09 +0800

    All functions finished

--- REST/src/Database.java
+++ REST/src/Database.java
@@ -19,25 +19,38 @@ public class Database {
 	@Path("{student}+{mark}")
 	public String addRecord(@PathParam("student") String student, @PathParam("mark") String mark) {
 	// Add a record using id as key and mark as value
+		if(table.containsKey(student)) {
+			return String.format("Record of %s exists", student);
+		}
 		table.put(student, Integer.valueOf(mark));
 		System.out.println("Student: " + student);
 		System.out.println("Mark: " + mark);
-		return "";
+		return String.format("Record of %s added", student);
 	}
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getMark(@QueryParam("student") String student) {
 	// Retrieve a record using id as key
-		if(table.contains(student)) {
-			System.out.println(student + " " + table.get(student));
-			return Integer.toString(table.get(student));
+		if(!table.contains(student)) {
+			System.out.println("Student not exists");
+			return String.format("Record of %s not exists", student);
 		}
-		System.out.println("Student not exists");
-		return "";
+		System.out.println(String.format("%s: %s", student, table.get(student)));
+		return String.format("Mark of %s: %s", student, Integer.toString(table.get(student)));
 	}
 	
-	public String updateRecord(. . .) {
+	@POST
+	@Path("{student}+{mark}")
+	public String updateRecord(@PathParam("student") String student, @PathParam("mark") String mark) {
 	// Update a record using id as key and mark as value
+		if(!table.contains(student)) {
+			System.out.println("Student not exists");
+			return String.format("Record of %s not exists", student);
+		}
+		table.replace(student, Integer.valueOf(mark));
+		System.out.println("Student: " + student);
+		System.out.println("Mark: " + mark);
+		return String.format("Record of %s updated", student);
 	}
 }
